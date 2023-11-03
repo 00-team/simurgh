612a5ee0a33945e929d81a6a54fe26895eb9fa04
sadra <sadrataghavi1383@gmail.com>
2023-11-03 13:04:24 +0330

    feat(dashboardf)

--- app/pages/dashboard/index.tsx
+++ app/pages/dashboard/index.tsx
@@ -3,7 +3,12 @@ import { Component } from 'solid-js'
 import './style/dashboard.scss'
 
 const Dashboard: Component = () => {
-    return <main class='dashboard'></main>
+    return (
+        <main class='dashboard'>
+            <aside class='sidebar'></aside>
+            <aside class='wrapper'></aside>
+        </main>
+    )
 }
 
 export default Dashboard
