89747baabe0e35f16d69296ee943b0c75e08b89c
sadra <sadrataghavi1383@gmail.com>
2023-11-08 13:33:59 +0330

    feat(projects):  init

---
+++ app/pages/dashboard/Projects.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'solid-js'
+
+import './style/projects.scss'
+
+const Projects: Component = () => {
+    return (
+        <section class='projects'>
+            <div class='project-card'>project</div>
+        </section>
+    )
+}
+
+export default Projects
