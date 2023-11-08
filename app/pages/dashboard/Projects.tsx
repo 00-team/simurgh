5fa04d6b8691d40ed400819b53d3d58a38ebb0fa
sadra <sadrataghavi1383@gmail.com>
2023-11-08 13:55:26 +0330

    feat(projects)

--- app/pages/dashboard/Projects.tsx
+++ app/pages/dashboard/Projects.tsx
@@ -2,15 +2,17 @@ import { Component } from 'solid-js'
 
 import './style/projects.scss'
 
-const Projects: Component = () => {
+export const Projects: Component = () => {
     return (
         <section class='projects'>
             <header class='section_title'>projects</header>
             <div class='projects-wrapper'>
                 <div class='project-card'></div>
+                <div class='project-card'></div>
+                <div class='project-card'></div>
+                <div class='project-card'></div>
+                <div class='project-card'></div>
             </div>
         </section>
     )
 }
-
-export default Projects
