b2c2bf3b4ef9307804c710b35165a263d7efe9e1
sadra <sadrataghavi1383@gmail.com>
2023-11-08 16:47:05 +0330

    feat(dashboard)

--- app/pages/dashboard/Projects.tsx
+++ app/pages/dashboard/Projects.tsx
@@ -3,15 +3,43 @@ import { Component } from 'solid-js'
 import './style/projects.scss'
 
 export const Projects: Component = () => {
+    // TODO: uncomment this later
+    // onMount(() => {
+    //     if (!user || !user.token) return
+
+    //     httpx({
+    //         url: '/api/verification/',
+    //         method: 'POST',
+    //         type: 'json',
+    //         headers: {
+    //             authorization: user.token,
+    //         },
+    //         onLoad(x) {
+    //             console.log(x)
+    //         },
+    //     })
+    // })
+
+    // type ProjectModel = {
+    //     project_id: number
+    //     title: string
+    //     description: string
+    //     features: string[]
+    //     sector: string
+    //     latitude: number
+    //     longitude: number
+    //     payment_terms: string
+    //     prices: PriceModel[]
+    //     images: ImagesModel
+    // }
+
     return (
         <section class='projects'>
             <header class='section_title'>projects</header>
             <div class='projects-wrapper'>
-                <div class='project-card'></div>
-                <div class='project-card'></div>
-                <div class='project-card'></div>
-                <div class='project-card'></div>
-                <div class='project-card'></div>
+                <div class='project-card'>
+                    <h3 class='title'>lorem ipsum</h3>
+                </div>
             </div>
         </section>
     )
