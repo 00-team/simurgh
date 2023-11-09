d74d37abee9d84b44660f5557161fe0f81b667e0
sadra <sadrataghavi1383@gmail.com>
2023-11-09 13:43:23 +0330

    feat(projectt)

---
+++ app/pages/dashboard/project.tsx
@@ -0,0 +1,7 @@
+import { Component } from 'solid-js'
+
+import './style/project.scss'
+
+export const Project: Component = ({}) => {
+    return <section class='project-container'>project</section>
+}
