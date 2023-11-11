126cde2bf850384de5b065d1650603d4c8a9f331
sadra <sadrataghavi1383@gmail.com>
2023-11-11 17:04:05 +0330

    feat(dashbboard)

--- app/pages/dashboard/project.tsx
+++ app/pages/dashboard/project.tsx
@@ -1,7 +1,11 @@
+import { useParams } from '@solidjs/router'
 import { Component } from 'solid-js'
 
 import './style/project.scss'
 
 export const Project: Component = ({}) => {
+    const param = useParams<{ id: string }>()
+
+    console.log(param.id)
     return <section class='project-container'>project</section>
 }
