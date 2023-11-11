8659c26e8fedb20b6350be2bbe0b09055c3e23f0
007 <dr007cc@gmail.com>
2023-11-11 12:59:06 +0330

    fix: update stuff

--- app/pages/blog-editor.tsx
+++ app/pages/blog-editor.tsx
@@ -1,45 +1,96 @@
-import { createStore, produce } from 'solid-js/store'
+import { SetStoreFunction, createStore, produce } from 'solid-js/store'
 import './style/blog-editor.scss'
-import { onCleanup, onMount } from 'solid-js'
+import { Component, createSignal, onMount } from 'solid-js'
+import { ImageIcon, TextIcon } from '!/icons/editor'
+// import { onCleanup, onMount } from 'solid-js'
+
+type EmptyBlock = {
+    type: 'empty'
+}
+
+type TextBlock = {
+    type: 'text'
+    html: string
+}
+
+type ImageBlock = {
+    type: 'image'
+    url: string
+    record_id: number
+}
+
+type Block = TextBlock | ImageBlock | EmptyBlock
+
+const DEFAULT_BLOCKS: { [T in Block as T['type']]: T } = {
+    empty: { type: 'empty' },
+    text: { type: 'text', html: '' },
+    image: { type: 'image', url: '', record_id: -1 },
+}
+
+type BlockCosmetic = {
+    title: string
+    icon: Component
+}
+
+type BlockCosmeticMap = Omit<
+    { [T in Block as T['type']]: BlockCosmetic },
+    'empty'
+>
+const BLOCK_COSMETIC: BlockCosmeticMap = {
+    text: { title: 'Text', icon: TextIcon },
+    image: { title: 'Image', icon: ImageIcon },
+}
+
+type StateModel = {
+    blocks: Block[]
+    active_block: number
+}
 
 // let observer: MutationObserver
 export default () => {
     let editor: HTMLDivElement
-    const [state, setState] = createStore({
-        blocks: [{}, {}],
+    const [state, setState] = createStore<StateModel>({
+        blocks: [
+            { type: 'text', html: '' },
+            DEFAULT_BLOCKS.empty,
+            { type: 'text', html: 'asdasd<br/>sss' },
+        ],
+        active_block: 0,
     })
 
-    onMount(() => {
-        // observer = new MutationObserver(() => {
-        //     setState({ html: editor.innerHTML })
-        // })
-        //
-        // observer.observe(editor, {
-        //     subtree: true,
-        //     childList: true,
-        //     characterData: true,
-        // })
-    })
-
-    onCleanup(() => {
-        // observer.disconnect()
-    })
+    // onMount(() => {})
+    // onCleanup(() => {})
 
     return (
         <div class='blog-editor-fnd'>
             <div class='header'>Header</div>
             <div class='content'>
                 <div class='editor' ref={editor}>
-                    {state.blocks.map((_, i) => (
-                        <div class='block'>
-                            <p contenteditable>block number: {i}</p>
+                    {state.blocks.map((b, i) => (
+                        <div
+                            class='block'
+                            classList={{
+                                active: i == state.active_block,
+                                [b.type]: true,
+                            }}
+                            onMouseDown={() => setState({ active_block: i })}
+                        >
+                            {block_map[b.type]({
+                                setState,
+                                state,
+                                block: b as never,
+                                id: i,
+                            })}
                         </div>
                     ))}
                     <button
                         onclick={() => {
                             setState(
                                 produce(s => {
-                                    s.blocks.push({})
+                                    let new_id = s.blocks.push({
+                                        type: 'empty',
+                                    })
+                                    s.active_block = new_id - 1
                                 })
                             )
                         }}
@@ -54,4 +105,75 @@ export default () => {
             </div>
         </div>
     )
+}
+
+type BlockComponent<T extends Block> = Component<{
+    state: StateModel
+    setState: SetStoreFunction<StateModel>
+    block: T
+    id: number
+}>
+
+type BlockMap = {
+    [T in Block as T['type']]: BlockComponent<T>
+}
+
+const EmptyComp: BlockComponent<EmptyBlock> = props => {
+    return (
+        <>
+            <span>Select the Block Type:</span>
+            <div class='blocks'>
+                {Object.entries(BLOCK_COSMETIC).map(([t, c]) => (
+                    <button
+                        class='select-btn'
+                        onclick={() => {
+                            props.setState(
+                                produce(s => {
+                                    s.blocks[props.id] = DEFAULT_BLOCKS[t]
+                                })
+                            )
+                        }}
+                    >
+                        {c.icon({})}
+                        {c.title}
+                    </button>
+                ))}
+            </div>
+        </>
+    )
+}
+
+const TextComp: BlockComponent<TextBlock> = props => {
+    const [placeholder, setPlaceholder] = createSignal(!props.block.html)
+
+    return (
+        <>
+            {placeholder() && <span class='placeholder'>Enter Text Here</span>}
+            <p
+                onInput={e => {
+                    setPlaceholder(!e.currentTarget.innerHTML)
+                }}
+                contenteditable={true}
+                onBlur={e => {
+                    props.setState(
+                        produce(s => {
+                            // @ts-ignore
+                            s.blocks[props.id].html = e.currentTarget.innerHTML
+                        })
+                    )
+                }}
+                innerHTML={props.block.html}
+            ></p>
+        </>
+    )
+}
+
+const ImageComp: BlockComponent<ImageBlock> = props => {
+    return <>IMAGE</>
+}
+
+const block_map: BlockMap = {
+    empty: EmptyComp,
+    text: TextComp,
+    image: ImageComp,
 }
