602feeb0afaff108d29f3f50d0973927dc678d75
sadra <sadrataghavi1383@gmail.com>
2023-11-12 11:48:58 +0330

    feat(dashboard): icon

--- app/icons/dashboard.tsx
+++ app/icons/dashboard.tsx
@@ -15,6 +15,20 @@ export const ProjectIcon = ({ size = 25 }) => (
     </svg>
 )
 
+export const PersonIcon = ({ size = 25 }) => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 16 16'
+        height={size}
+        width={size}
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path d='M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z'></path>
+    </svg>
+)
+
 export const BlogIcon = ({ size = 25 }) => (
     <svg
         stroke='currentColor'
