0bbc13680f3b6373d031979a3d6e9b4009b760fe
007 <dr007cc@gmail.com>
2024-05-28 20:15:49 +0330

    feat(app): add self store

---
+++ app/store/self.ts
@@ -0,0 +1,60 @@
+import { UserModel } from 'models'
+import { httpx } from 'shared'
+import { createEffect, createRoot } from 'solid-js'
+
+import { createStore } from 'solid-js/store'
+
+type SelfModel = {
+    user: UserModel
+    loged_in: boolean
+    fetch: boolean
+}
+
+async function get_default(): Promise<SelfModel> {
+    try {
+        const result: any = await new Promise((resolve, reject) => {
+            httpx({
+                url: '/api/user/',
+                method: 'GET',
+                reject,
+                onLoad(x) {
+                    if (x.status != 200) return reject()
+                    resolve(x.response)
+                },
+            })
+        })
+
+        return {
+            user: result,
+            loged_in: true,
+            fetch: false,
+        }
+    } catch {}
+
+    return {
+        loged_in: false,
+        fetch: true,
+        user: {
+            id: 0,
+            name: '',
+            token: '',
+            photo: null,
+            admin: false,
+            banned: false,
+            client: false,
+            email: '',
+        },
+    }
+}
+
+const [self, setSelf] = createStore(await get_default())
+
+createRoot(() => {
+    createEffect(() => {
+        if (!self.fetch && self.loged_in) return
+
+        get_default().then(result => setSelf(result))
+    })
+})
+
+export { self, setSelf }
