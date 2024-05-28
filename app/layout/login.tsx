224e7b9748ff201c4d44d78409378f4f64b206b5
007 <dr007cc@gmail.com>
2024-05-28 20:35:40 +0330

    feat(app): add some basic elements in login

--- app/layout/login.tsx
+++ app/layout/login.tsx
@@ -1,5 +1,19 @@
 import './style/login.scss'
 
 export default () => {
-    return <div class='login-fnd'>login</div>
+    return (
+        <div class='login-fnd'>
+            <div class='login-card'>
+                <div class='logo'>Logo</div>
+                <div class='login-form'>
+                    <div class='row'>
+                        <label for='login-email'>ایمیل:</label>
+                        <input id='login-email' type='email' class='styled' />
+                    </div>
+                    <div class='row'>pin</div>
+                    <button class='styled'>action</button>
+                </div>
+            </div>
+        </div>
+    )
 }
