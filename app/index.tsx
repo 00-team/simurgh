e2c3b0d1195e43daea1b1afc98d13fee663a299d
007 <dr007cc@gmail.com>
2024-05-28 20:17:31 +0330

    fix(app): update index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,17 @@
-import { render } from 'solid-js/web'
+import { Show, render } from 'solid-js/web'
 
 import './style/index.scss'
+import { self } from 'store'
+import Login from 'layout/login'
+import Sidebar from 'layout/sidebar'
 
 const Root = () => {
-    return <div></div>
+    return (
+        <Show when={self.loged_in} fallback={<Login />}>
+            app
+            <Sidebar />
+        </Show>
+    )
 }
 
 render(Root, document.getElementById('root'))
