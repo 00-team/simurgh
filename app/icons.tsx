2e731913b671607b45838a0652d46bdcd51a5fed
007 <dr007cc@gmail.com>
2024-05-28 18:20:02 +0330

    feat(app): add some icon

---
+++ app/icons.tsx
@@ -0,0 +1,198 @@
+export const CircleAlertIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='12' r='10' />
+        <path d='M12 16v-4' />
+        <path d='M12 8h.01' />
+    </svg>
+)
+
+export const CircleCheckIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='12' r='10' />
+        <path d='m9 12 2 2 4-4' />
+    </svg>
+)
+
+export const CircleXIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='12' r='10' />
+        <path d='m15 9-6 6' />
+        <path d='m9 9 6 6' />
+    </svg>
+)
+
+export const XIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        stroke-width='1.5'
+        stroke='currentColor'
+    >
+        <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M6 18L18 6M6 6l12 12'
+        />
+    </svg>
+)
+
+export const UserIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='8' r='5' />
+        <path d='M20 21a8 8 0 0 0-16 0' />
+    </svg>
+)
+
+export const ChevronLeftIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m15 18-6-6 6-6' />
+    </svg>
+)
+
+export const ChevronRightIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m9 18 6-6-6-6' />
+    </svg>
+)
+
+export const ChevronDownIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m6 9 6 6 6-6' />
+    </svg>
+)
+
+export const ChevronUpIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m18 15-6-6-6 6' />
+    </svg>
+)
+
+export const TrashIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M3 6h18' />
+        <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+        <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+        <line x1='10' x2='10' y1='11' y2='17' />
+        <line x1='14' x2='14' y1='11' y2='17' />
+    </svg>
+)
+
+export const BanIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='12' r='10' />
+        <path d='m4.9 4.9 14.2 14.2' />
+    </svg>
+)
+
+export const CircleCheckBigIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M22 11.08V12a10 10 0 1 1-5.93-9.14' />
+        <path d='m9 11 3 3L22 4' />
+    </svg>
+)
+
+export const HourglassIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M5 22h14' />
+        <path d='M5 2h14' />
+        <path d='M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22' />
+        <path d='M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2' />
+    </svg>
+)
