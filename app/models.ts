72978ed672502b3bc0cffdcb840fbe138a87d827
007 <dr007cc@gmail.com>
2024-05-28 17:56:25 +0330

    feat(app): add UserModel in models.ts

---
+++ app/models.ts
@@ -0,0 +1,10 @@
+export type UserModel = {
+    id: number
+    email: string
+    name?: string | null
+    photo?: string | null
+    token: string
+    admin: boolean
+    client: boolean
+    banned: boolean
+}
