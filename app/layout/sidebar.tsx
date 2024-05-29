c004d98db0a95f0eacd6c9c9e2174108c3eef0c0
007 <dr007cc@gmail.com>
2024-05-29 15:17:55 +0330

    feat(app): add name editor in sidebar

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -3,9 +3,17 @@ import './style/sidebar.scss'
 import { self, setSelf } from 'store'
 import { UserIcon } from 'icons'
 import { httpx } from 'shared'
-import { produce } from 'solid-js/store'
+import { createStore, produce } from 'solid-js/store'
 
 export default () => {
+    type State = {
+        name_edit: boolean
+    }
+
+    const [state, setState] = createStore<State>({
+        name_edit: false,
+    })
+
     function update_photo() {
         let input = document.createElement('input')
         input.setAttribute('type', 'file')
@@ -53,6 +61,20 @@ export default () => {
         })
     }
 
+    function update_name(name: string) {
+        httpx({
+            url: '/api/user/',
+            method: 'PATCH',
+            json: { name },
+            onLoad(x) {
+                if (x.status == 200) {
+                    setSelf({ user: x.response })
+                    setState({ name_edit: false })
+                }
+            },
+        })
+    }
+
     return (
         <div class='sidebar-fnd'>
             <div class='user'>
@@ -72,7 +94,31 @@ export default () => {
                         />
                     </Show>
                 </div>
-                <span class='name'>{self.user.name || '---'}</span>
+                <div class='name' onclick={() => setState({ name_edit: true })}>
+                    <Show
+                        when={state.name_edit}
+                        fallback={<span>{self.user.name || '---'}</span>}
+                    >
+                        <input
+                            dir='auto'
+                            class='styled'
+                            placeholder='نام شما'
+                            value={self.user.name || ''}
+                            maxLength={256}
+                            onChange={e => {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                update_name(e.currentTarget.value)
+                            }}
+                            onBlur={() => setState({ name_edit: false })}
+                            onContextMenu={e => {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                setState({ name_edit: false })
+                            }}
+                        />
+                    </Show>
+                </div>
             </div>
         </div>
     )
