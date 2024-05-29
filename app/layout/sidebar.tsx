3d5c441a905d555a8bf48e0fdc28446c85bbf23d
007 <dr007cc@gmail.com>
2024-05-29 14:29:55 +0330

    feat(app): delete user profile on context menu

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -1,12 +1,30 @@
 import { Show } from 'solid-js'
 import './style/sidebar.scss'
-import { self } from 'store'
+import { self, setSelf } from 'store'
 import { UserIcon } from 'icons'
+import { httpx } from 'shared'
+import { produce } from 'solid-js/store'
 
 export default () => {
     function update_photo() {}
 
-    function delete_photo() {}
+    function delete_photo() {
+        if (!self.user.photo) return
+
+        httpx({
+            url: '/api/user/photo/',
+            method: 'DELETE',
+            onLoad(x) {
+                if (x.status == 200) {
+                    setSelf(
+                        produce(s => {
+                            s.user.photo = null
+                        })
+                    )
+                }
+            },
+        })
+    }
 
     return (
         <div class='sidebar-fnd'>
