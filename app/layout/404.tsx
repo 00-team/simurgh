3c66b7235492825331c9040b6fde1a3797c0ea87
007 <dr007cc@gmail.com>
2024-05-29 17:29:18 +0330

    feat(app): add home url in 404

--- app/layout/404.tsx
+++ app/layout/404.tsx
@@ -1,9 +1,11 @@
+import { A } from '@solidjs/router'
 import './style/404.scss'
 
 export default () => {
     return (
         <div class='not-found-fnd'>
             <h1>صفحه مورد نظر شما پیدا نشد!</h1>
+            <A href='/'>خانه</A>
         </div>
     )
 }
