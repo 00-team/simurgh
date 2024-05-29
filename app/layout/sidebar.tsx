03d29585906ef22ef1fc58ce8bb349cecc38f8c3
007 <dr007cc@gmail.com>
2024-05-29 15:21:15 +0330

    refactor(app): move sidebar user section

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Show } from 'solid-js'
+import { Show, createSignal } from 'solid-js'
 import './style/sidebar.scss'
 import { self, setSelf } from 'store'
 import { UserIcon } from 'icons'
@@ -6,13 +6,15 @@ import { httpx } from 'shared'
 import { createStore, produce } from 'solid-js/store'
 
 export default () => {
-    type State = {
-        name_edit: boolean
-    }
+    return (
+        <div class='sidebar-fnd'>
+            <User />
+        </div>
+    )
+}
 
-    const [state, setState] = createStore<State>({
-        name_edit: false,
-    })
+const User = () => {
+    const [editing, setEditing] = createSignal(false)
 
     function update_photo() {
         let input = document.createElement('input')
@@ -69,56 +71,54 @@ export default () => {
             onLoad(x) {
                 if (x.status == 200) {
                     setSelf({ user: x.response })
-                    setState({ name_edit: false })
+                    setEditing(false)
                 }
             },
         })
     }
 
     return (
-        <div class='sidebar-fnd'>
-            <div class='user'>
-                <div
-                    class='img'
-                    onclick={update_photo}
-                    onContextMenu={e => {
-                        e.preventDefault()
-                        e.stopPropagation()
-                        delete_photo()
-                    }}
+        <div class='user'>
+            <div
+                class='img'
+                onclick={update_photo}
+                onContextMenu={e => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    delete_photo()
+                }}
+            >
+                <Show when={self.user.photo} fallback={<UserIcon />}>
+                    <img
+                        src={`/record/${self.user.id}:${self.user.photo}/?x=${~~performance.now()}`}
+                        draggable={false}
+                    />
+                </Show>
+            </div>
+            <div class='name' onclick={() => setEditing(true)}>
+                <Show
+                    when={editing()}
+                    fallback={<span>{self.user.name || '---'}</span>}
                 >
-                    <Show when={self.user.photo} fallback={<UserIcon />}>
-                        <img
-                            src={`/record/${self.user.id}:${self.user.photo}/?hash=${performance.now()}`}
-                            draggable={false}
-                        />
-                    </Show>
-                </div>
-                <div class='name' onclick={() => setState({ name_edit: true })}>
-                    <Show
-                        when={state.name_edit}
-                        fallback={<span>{self.user.name || '---'}</span>}
-                    >
-                        <input
-                            dir='auto'
-                            class='styled'
-                            placeholder='نام شما'
-                            value={self.user.name || ''}
-                            maxLength={256}
-                            onChange={e => {
-                                e.preventDefault()
-                                e.stopPropagation()
-                                update_name(e.currentTarget.value)
-                            }}
-                            onBlur={() => setState({ name_edit: false })}
-                            onContextMenu={e => {
-                                e.preventDefault()
-                                e.stopPropagation()
-                                setState({ name_edit: false })
-                            }}
-                        />
-                    </Show>
-                </div>
+                    <input
+                        dir='auto'
+                        class='styled'
+                        placeholder='نام شما'
+                        value={self.user.name || ''}
+                        maxLength={256}
+                        onChange={e => {
+                            e.preventDefault()
+                            e.stopPropagation()
+                            update_name(e.currentTarget.value)
+                        }}
+                        onBlur={() => setEditing(false)}
+                        onContextMenu={e => {
+                            e.preventDefault()
+                            e.stopPropagation()
+                            setEditing(false)
+                        }}
+                    />
+                </Show>
             </div>
         </div>
     )
