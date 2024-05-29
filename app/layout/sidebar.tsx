f574f5312221d8e79a536c7f35d7f703ee707ddd
007 <dr007cc@gmail.com>
2024-05-29 14:28:12 +0330

    feat(app): add user info

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -1,5 +1,34 @@
+import { Show } from 'solid-js'
 import './style/sidebar.scss'
+import { self } from 'store'
+import { UserIcon } from 'icons'
 
 export default () => {
-    return <div class='sidebar-fnd'>sidebar</div>
+    function update_photo() {}
+
+    function delete_photo() {}
+
+    return (
+        <div class='sidebar-fnd'>
+            <div class='user'>
+                <div
+                    class='img'
+                    onclick={update_photo}
+                    onContextMenu={e => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        delete_photo()
+                    }}
+                >
+                    <Show when={self.user.photo} fallback={<UserIcon />}>
+                        <img
+                            src={`/record/${self.user.id}:${self.user.photo}/`}
+                            draggable={false}
+                        />
+                    </Show>
+                </div>
+                <span class='name'>{self.user.name || '---'}</span>
+            </div>
+        </div>
+    )
 }
