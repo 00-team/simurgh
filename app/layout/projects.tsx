aaf4f4eb9024da7fe169441886f3fb6b1fb5f90e
007 <dr007cc@gmail.com>
2024-06-01 13:18:00 +0330

    fix(app): add project_new func

--- app/layout/projects.tsx
+++ app/layout/projects.tsx
@@ -1,7 +1,7 @@
 import { useParams } from '@solidjs/router'
 import './style/projects.scss'
 import { ProjectModel } from 'models'
-import { createStore } from 'solid-js/store'
+import { createStore, produce } from 'solid-js/store'
 import { fmt_bytes, fmt_datetime, httpx } from 'shared'
 import { createEffect } from 'solid-js'
 
@@ -14,10 +14,10 @@ export default () => {
     const UP = useParams()
 
     createEffect(() => {
-        fetch_projects(parseInt(UP.page || '0') || 0)
+        projects_list(parseInt(UP.page || '0') || 0)
     })
 
-    function fetch_projects(page: number) {
+    function projects_list(page: number) {
         httpx({
             url: '/api/projects/',
             method: 'GET',
@@ -29,37 +29,40 @@ export default () => {
         })
     }
 
+    function projects_new() {
+        httpx({
+            url: '/api/projects/',
+            method: 'POST',
+            json: { name: 'پروژه جدید' },
+            onLoad(x) {
+                if (x.status != 200) return
+                projects_list(state.page)
+            },
+        })
+    }
+
     return (
         <div class='projects-fnd'>
             <div class='actions'>
-                <button class='styled'>پروژه جدید</button>
+                <button class='styled' onClick={projects_new}>
+                    پروژه جدید
+                </button>
             </div>
             <div class='project-list'>
-                {state.projects
-                    .concat(state.projects)
-                    .concat(state.projects)
-                    .concat(state.projects)
-                    .concat(state.projects)
-                    .concat(state.projects)
-                    .concat(state.projects)
-                    .map(p => (
-                        <div class='project'>
-                            <span>نام:</span>
-                            <span>{p.name}</span>
-                            <span>بلاگ ها:</span>
-                            <span class='n'>
-                                {p.blog_count.toLocaleString()}
-                            </span>
-                            <span>فایل ها:</span>
-                            <span class='n'>
-                                {p.record_count.toLocaleString()}
-                            </span>
-                            <span>فضا:</span>
-                            <span class='n'>{fmt_bytes(p.storage)}</span>
-                            <span>تاریخ:</span>
-                            <span class='n'>{fmt_datetime(p.timestamp)}</span>
-                        </div>
-                    ))}
+                {state.projects.map(p => (
+                    <div class='project'>
+                        <span>نام:</span>
+                        <span>{p.name}</span>
+                        <span>بلاگ ها:</span>
+                        <span class='n'>{p.blog_count.toLocaleString()}</span>
+                        <span>فایل ها:</span>
+                        <span class='n'>{p.record_count.toLocaleString()}</span>
+                        <span>فضا:</span>
+                        <span class='n'>{fmt_bytes(p.storage)}</span>
+                        <span>تاریخ:</span>
+                        <span class='n'>{fmt_datetime(p.timestamp)}</span>
+                    </div>
+                ))}
             </div>
         </div>
     )
