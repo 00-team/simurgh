0441d7f0361780c3ac589cbac918ab0c29d51e7f
007 <dr007cc@gmail.com>
2024-06-13 19:59:10 +0330

    feat(app): delete action for projects

--- app/layout/project.tsx
+++ app/layout/project.tsx
@@ -4,6 +4,8 @@ import { createStore, produce } from 'solid-js/store'
 import { Show, createEffect } from 'solid-js'
 import { useNavigate, useParams } from '@solidjs/router'
 import { fmt_bytes, fmt_datetime, httpx } from 'shared'
+import { Confact } from 'comps'
+import { TrashIcon } from 'icons'
 
 export default () => {
     type State = {
@@ -67,41 +69,60 @@ export default () => {
         })
     }
 
+    function project_delete() {
+        httpx({
+            url: `/api/projects/${state.project.id}/`,
+            method: 'DELETE',
+            onLoad(x) {
+                if (x.status != 200) return
+                nav('/projects/')
+            },
+        })
+    }
+
     return (
         <div class='project-fnd'>
             <div class='info'>
                 <span>نام:</span>
-                <Show
-                    when={state.edit_name}
-                    fallback={
-                        <span
-                            style={{ cursor: 'pointer' }}
-                            onClick={() => setState({ edit_name: true })}
-                        >
-                            {state.project.name}
-                        </span>
-                    }
-                >
-                    <input
-                        class='styled'
-                        ref={input_name}
-                        value={state.project.name}
-                        dir='auto'
-                        placeholder='نام شما'
-                        maxLength={256}
-                        onChange={e => {
-                            e.preventDefault()
-                            e.stopPropagation()
-                            project_update({ name: e.currentTarget.value })
-                        }}
-                        onBlur={() => setState({ edit_name: false })}
-                        onContextMenu={e => {
-                            e.preventDefault()
-                            e.stopPropagation()
-                            setState({ edit_name: false })
-                        }}
+                <div class='name'>
+                    <Show
+                        when={state.edit_name}
+                        fallback={
+                            <span
+                                style={{ cursor: 'pointer' }}
+                                onClick={() => setState({ edit_name: true })}
+                            >
+                                {state.project.name}
+                            </span>
+                        }
+                    >
+                        <input
+                            class='styled'
+                            ref={input_name}
+                            value={state.project.name}
+                            dir='auto'
+                            placeholder='نام شما'
+                            maxLength={256}
+                            onChange={e => {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                project_update({ name: e.currentTarget.value })
+                            }}
+                            onBlur={() => setState({ edit_name: false })}
+                            onContextMenu={e => {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                setState({ edit_name: false })
+                            }}
+                        />
+                    </Show>
+                    <Confact
+                        icon={TrashIcon}
+                        onAct={project_delete}
+                        color='var(--red)'
+                        timer_ms={2e3}
                     />
-                </Show>
+                </div>
 
                 <span>بلاگ ها:</span>
                 <span class='n'>
