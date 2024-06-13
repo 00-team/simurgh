6f0dd86f76a59e60908a937e5b7cbc62bc0a7e29
007 <dr007cc@gmail.com>
2024-06-13 19:56:36 +0330

    feat(app): update confact comp

--- app/comps/confact.tsx
+++ app/comps/confact.tsx
@@ -1,23 +1,35 @@
 // conf-act or confirm action :)
 
 import './style/confact.scss'
-import { Component, JSX, createSignal } from 'solid-js'
+import { Component, JSX, createEffect, createSignal } from 'solid-js'
 
 type Props = {
     icon: () => JSX.Element
     timer_ms?: number
     onAct(): void
     color: string
+    disabled?: boolean
 }
 const Confact: Component<Props> = P => {
     const [stage2, setStage2] = createSignal(false)
     let timeout = 0
+    let timer = 0
+
+    createEffect(() => {
+        clearTimeout(timer)
+        if (!stage2()) return
+        timer = setTimeout(() => {
+            setStage2(false)
+        }, 10e3)
+    })
 
     return (
         <button
+            disabled={P.disabled}
             style={{ '--tsd': (P.timer_ms || 1000) + 'ms', '--color': P.color }}
             class='cmp-confact icon'
             classList={{ stage2: stage2() }}
+            title={stage2() ? 'right click to unactivate' : 'hold to activate'}
             oncontextmenu={e => {
                 e.preventDefault()
                 setStage2(false)
