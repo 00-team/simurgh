69d8d093abd89ae0cc67dac91d0ba63b2154e8c1
007 <dr007cc@gmail.com>
2024-06-15 12:16:43 +0330

    feat(api): impl FromRequest for Record

--- src/models/record.rs
+++ src/models/record.rs
@@ -1,5 +1,9 @@
+use actix_web::{dev::Payload, web::Data, HttpRequest};
 use serde::{Deserialize, Serialize};
+use std::{future::Future, pin::Pin};
 use utoipa::ToSchema;
+
+use super::AppErrNotFound;
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow, ToSchema, Default)]
 pub struct Record {
@@ -13,4 +17,34 @@ pub struct Record {
     pub ext: Option<String>,
 }
 
-super::from_request!(Record, "records");
+impl actix_web::FromRequest for Record {
+    type Error = crate::models::AppErr;
+    type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
+
+    fn from_request(rq: &HttpRequest, pl: &mut Payload) -> Self::Future {
+        let user = super::user::User::from_request(rq, pl);
+        let project = super::project::Project::from_request(rq, pl);
+        let path = actix_web::web::Path::<(i64, i64)>::extract(rq);
+        let state = rq.app_data::<Data<crate::AppState>>().unwrap();
+        let pool = state.sql.clone();
+
+        Box::pin(async move {
+            let user = user.await?;
+            let path = path.await?;
+            let project = project.await?;
+            let record = sqlx::query_as! {
+                Record,
+                "select * from records where id = ?",
+                path.1
+            }
+            .fetch_one(&pool)
+            .await?;
+
+            if record.project == Some(project.id) || user.admin {
+                Ok(record)
+            } else {
+                Err(AppErrNotFound("یافت نشد"))
+            }
+        })
+    }
+}
