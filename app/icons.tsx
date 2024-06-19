fcefea5992364a9bbf85ccb09c2670f5aca4510b
007 <dr007cc@gmail.com>
2024-06-20 03:14:56 +0330

    feat(app): plus icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -282,3 +282,18 @@ export const FileIcon = () => (
         <path d='M14 2v4a2 2 0 0 0 2 2h4' />
     </svg>
 )
+
+export const PlusIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M5 12h14' />
+        <path d='M12 5v14' />
+    </svg>
+)
