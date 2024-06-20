85b3533b038e34a524f174845719e8bda4e5a81c
007 <dr007cc@gmail.com>
2024-06-20 10:01:04 +0330

    feat(app): add onClick for Editable

--- app/comps/editable.tsx
+++ app/comps/editable.tsx
@@ -4,11 +4,12 @@ import './style/editable.scss'
 
 type Props = {
     children: JSX.Element
+    onClick?(): void
 }
 
 export const Editable: Component<Props> = P => {
     return (
-        <div class='cmp-editable' title='click to edit'>
+        <div class='cmp-editable' title='click to edit' onClick={P.onClick}>
             <div class='cmp-editable-icon'>
                 <PencilIcon />
             </div>
