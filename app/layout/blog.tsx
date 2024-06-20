9be302ba43fe43e9864f06e55c2dc25f550bf15c
007 <dr007cc@gmail.com>
2024-06-20 12:53:36 +0330

    feat(app): blog page

--- app/layout/blog.tsx
+++ app/layout/blog.tsx
@@ -1,3 +1,29 @@
+import { createStore } from 'solid-js/store'
+import './style/blog.scss'
+import { BlogModel } from 'models'
+
 export default () => {
+    type State = {
+        blog: BlogModel
+        edit: boolean
+    }
+    const [state, setState] = createStore<State>({
+        blog: {
+            id: 0,
+            slug: '',
+            project: null,
+            author: null,
+            created_at: 0,
+            updated_at: 0,
+            title: '',
+            detail: '',
+            data: '',
+            html: '',
+            thumbnail: null,
+            read_time: null,
+        },
+        edit: false,
+    })
+
     return <div>blog page</div>
 }
