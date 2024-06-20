be223059033b001122c04dee6d8a85518019e50f
007 <dr007cc@gmail.com>
2024-06-20 23:13:58 +0330

    feat(app): reset icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -342,3 +342,18 @@ export const SaveIcon = () => (
         <path d='M7 3v4a1 1 0 0 0 1 1h7' />
     </svg>
 )
+
+export const RotateCcwIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8' />
+        <path d='M3 3v5h5' />
+    </svg>
+)
