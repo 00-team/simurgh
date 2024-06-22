e4a97ac329c53ca78c08a01f70f6a6938a9692ea
007 <dr007cc@gmail.com>
2024-06-22 05:08:48 +0330

    feat(app): eye icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -440,3 +440,18 @@ export const SplitIcon = () => (
         <polyline points='16 16 20 12 16 8' />
     </svg>
 )
+
+export const EyeIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z' />
+        <circle cx='12' cy='12' r='3' />
+    </svg>
+)
