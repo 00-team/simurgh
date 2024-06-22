d6bfe3e52caa11ab91f76a0c058667f741cf85c1
007 <dr007cc@gmail.com>
2024-06-22 08:26:05 +0330

    feat(app): a arrow icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -571,3 +571,37 @@ export const PilcrowRightIcon = () => (
         <path d='m22 18-4 4' />
     </svg>
 )
+
+export const AArrowDownIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M3.5 13h6' />
+        <path d='m2 16 4.5-9 4.5 9' />
+        <path d='M18 7v9' />
+        <path d='m14 12 4 4 4-4' />
+    </svg>
+)
+
+export const AArrowUpIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M3.5 13h6' />
+        <path d='m2 16 4.5-9 4.5 9' />
+        <path d='M18 16V7' />
+        <path d='m14 11 4-4 4 4' />
+    </svg>
+)
