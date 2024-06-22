78345cd35cd87bbbe2f79369666dac5801817d85
007 <dr007cc@gmail.com>
2024-06-22 05:32:37 +0330

    feat(app): underline icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -520,3 +520,18 @@ export const BoldIcon = () => (
         <path d='M6 12h9a4 4 0 0 1 0 8H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h7a4 4 0 0 1 0 8' />
     </svg>
 )
+
+export const UnderlineIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M6 4v6a6 6 0 0 0 12 0V4' />
+        <line x1='4' x2='20' y1='20' y2='20' />
+    </svg>
+)
