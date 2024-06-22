d12be34f79f850e228ddf60f10b8fb2c2753912a
007 <dr007cc@gmail.com>
2024-06-22 05:32:09 +0330

    feat(app): bold icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -506,3 +506,17 @@ export const ItalicIcon = () => (
         <line x1='15' x2='9' y1='4' y2='20' />
     </svg>
 )
+
+export const BoldIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M6 12h9a4 4 0 0 1 0 8H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h7a4 4 0 0 1 0 8' />
+    </svg>
+)
