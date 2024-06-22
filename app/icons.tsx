a0c121f2ee7826ec17a0c07f4d11d909e3049d85
007 <dr007cc@gmail.com>
2024-06-22 05:31:41 +0330

    feat(app): italic icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -490,3 +490,19 @@ export const PaletteIcon = () => (
         <path d='M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z' />
     </svg>
 )
+
+export const ItalicIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <line x1='19' x2='10' y1='4' y2='4' />
+        <line x1='14' x2='5' y1='20' y2='20' />
+        <line x1='15' x2='9' y1='4' y2='20' />
+    </svg>
+)
