be0339435bba0b9518be91e85937f661e8d8774a
007 <dr007cc@gmail.com>
2024-06-24 20:39:09 +0330

    feat(simple): type icon for font family

--- simple/icons.tsx
+++ simple/icons.tsx
@@ -699,3 +699,19 @@ export const LinkIcon = () => (
         <path d='M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71' />
     </svg>
 )
+
+export const TypeIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <polyline points='4 7 4 4 20 4 20 7' />
+        <line x1='9' x2='15' y1='20' y2='20' />
+        <line x1='12' x2='12' y1='4' y2='20' />
+    </svg>
+)
