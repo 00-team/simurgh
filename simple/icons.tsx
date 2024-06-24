614d781ddc3bf876fdd5b47aa54ff17132f390e8
007 <dr007cc@gmail.com>
2024-06-24 20:36:23 +0330

    feat(simple): marker icon

--- simple/icons.tsx
+++ simple/icons.tsx
@@ -669,3 +669,18 @@ export const CodeXmlIcon = () => (
         <path d='m14.5 4-5 16' />
     </svg>
 )
+
+export const HighlighterIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m9 11-6 6v3h9l3-3' />
+        <path d='m22 12-4.6 4.6a2 2 0 0 1-2.8 0l-5.2-5.2a2 2 0 0 1 0-2.8L14 4' />
+    </svg>
+)
