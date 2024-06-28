63016f61c3487d52f0241278d7f416284f916588
sadra <sadrataghavi1383@gmail.com>
2024-06-28 19:35:48 +0330

    feat(projects)

--- app/pages/projects.tsx
+++ app/pages/projects.tsx
@@ -82,25 +82,42 @@ export default () => {
                 <div class='projects-wrapper'>
                     {state.projects.map(p => (
                         <div
-                            class='project'
+                            class='project title_small'
                             onClick={() => nav('/projects/' + p.id)}
                         >
-                            <span>نام:</span>
-                            <span>{p.name}</span>
-                            <span>بلاگ ها:</span>
-                            <span class='n'>
-                                {p.blog_count.toLocaleString()}
-                            </span>
-                            <span>فایل ها:</span>
-                            <span class='n'>
-                                {p.record_count.toLocaleString()}
-                            </span>
-                            <span>فضا:</span>
-                            <span class='n'>{fmt_bytes(p.storage)}</span>
-                            <span>تاریخ شروع:</span>
-                            <span class='n'>{fmt_datetime(p.created_at)}</span>
-                            <span>تاریخ آپدیت:</span>
-                            <span class='n'>{fmt_datetime(p.updated_at)}</span>
+                            <div class='project-name title'>
+                                <span>{p.name}</span>
+                            </div>
+                            <div class='row'>
+                                <span>بلاگ ها:</span>
+                                <span class='n'>
+                                    {p.blog_count.toLocaleString()}
+                                </span>
+                            </div>
+                            <div class='row'>
+                                <span>فایل ها:</span>
+                                <span class='n'>
+                                    {p.record_count.toLocaleString()}
+                                </span>
+                            </div>
+                            <div class='row '>
+                                <span>فضا:</span>
+                                <span class='n space'>
+                                    {fmt_bytes(p.storage)}
+                                </span>
+                            </div>
+                            <div class='row'>
+                                <span>تاریخ شروع:</span>
+                                <span class='date'>
+                                    {fmt_datetime(p.created_at)}
+                                </span>
+                            </div>
+                            <div class='row'>
+                                <span>تاریخ آپدیت:</span>
+                                <span class='n'>
+                                    {fmt_datetime(p.updated_at)}
+                                </span>
+                            </div>
                         </div>
                     ))}
                 </div>
