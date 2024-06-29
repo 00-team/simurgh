eb3f90a483664351cb87b3f88754f6439f5ca04a
sadra <sadrataghavi1383@gmail.com>
2024-06-29 14:53:49 +0330

    feat(editable)

--- app/comps/editable.tsx
+++ app/comps/editable.tsx
@@ -5,11 +5,17 @@ import './style/editable.scss'
 type Props = {
     children: JSX.Element
     onClick?(): void
+    img?: boolean
 }
 
 export const Editable: Component<Props> = P => {
     return (
-        <div class='cmp-editable' title='click to edit' onClick={P.onClick}>
+        <div
+            class='cmp-editable'
+            style={{ padding: P.img ? 0 : '1em' }}
+            title='click to edit'
+            onClick={P.onClick}
+        >
             <div class='cmp-editable-icon'>
                 <PencilIcon />
             </div>
