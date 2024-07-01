d39db62befdbd2e822185ad53f91c8c1b8cfb286
007 <dr007cc@gmail.com>
2024-07-01 16:44:14 +0330

    feat(simple): popup comp and use it in tooltip

---
+++ simple/comps/popup.tsx
@@ -0,0 +1,69 @@
+import { Component, JSX, Show, createEffect } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { Portal } from 'solid-js/web'
+
+import './style/popup.scss'
+
+type ChildProps = {
+    parent: HTMLElement
+}
+
+type Props = {
+    show: boolean
+    children: JSX.Element
+    getProps(props: ChildProps): void
+}
+export const Popup: Component<Props> = P => {
+    let decoy: HTMLDivElement
+    let popup: HTMLDivElement
+
+    type State = {
+        x?: number
+        y?: number
+        p?: number
+        pb?: boolean
+    }
+    const [state, setState] = createStore<State>({
+        pb: false,
+    })
+
+    createEffect(() => {
+        if (!decoy) return
+        P.getProps({ parent: decoy.parentElement })
+    })
+
+    createEffect(() => {
+        if (!P.show) return
+        const p = decoy.parentElement.getBoundingClientRect()
+        setState({ x: ~~p.left, p: ~~(p.width / 2) })
+
+        if (!popup) return
+        const c = popup.getBoundingClientRect()
+        if (p.top < c.height + 10) {
+            setState({ y: ~~(p.bottom + 10), pb: true })
+        } else {
+            setState({ y: ~~(p.top - c.height - 10), pb: false })
+        }
+    })
+
+    return (
+        <div class='cmp-popup-decoy' ref={decoy}>
+            <Show when={P.show}>
+                <Portal>
+                    <div
+                        class='cmp-popup'
+                        ref={popup}
+                        classList={{ bottom: state.pb }}
+                        style={{
+                            top: state.y + 'px',
+                            left: state.x + 'px',
+                            '--after-offset': state.p + 'px',
+                        }}
+                    >
+                        {P.children}
+                    </div>
+                </Portal>
+            </Show>
+        </div>
+    )
+}
--- simple/comps/tooltip.tsx
+++ simple/comps/tooltip.tsx
@@ -1,72 +1,49 @@
-import { Component, JSX, Show, onCleanup, onMount } from 'solid-js'
-
-import './style/tooltip.scss'
+import { Component, JSX, createEffect, onCleanup } from 'solid-js'
+import { Popup } from './popup'
 import { createStore } from 'solid-js/store'
-import { Portal } from 'solid-js/web'
 
 type Props = {
     children: JSX.Element
 }
 export const Tooltip: Component<Props> = P => {
-    let decoy: HTMLDivElement
-    let tooltip: HTMLDivElement
-
     type State = {
         show: boolean
-        x?: number
-        y?: number
-        p?: number
-        pb?: boolean
+        parent?: HTMLElement
     }
     const [state, setState] = createStore<State>({
         show: false,
-        pb: false,
     })
-
-    function enter(e: { currentTarget: HTMLElement } & MouseEvent) {
-        const p = e.currentTarget.getBoundingClientRect()
-        setState({ show: true, x: ~~p.left, p: ~~(p.width / 2) })
-        if (!tooltip) return
-        const c = tooltip.getBoundingClientRect()
-        if (p.top < c.height + 10) {
-            setState({ y: ~~(p.bottom + 10), pb: true })
-        } else {
-            setState({ y: ~~(p.top - c.height - 10), pb: false })
-        }
-    }
 
     function leave() {
         setState({ show: false })
     }
 
-    onMount(() => {
-        decoy.parentElement.addEventListener('mouseenter', enter)
-        decoy.parentElement.addEventListener('mouseleave', leave)
+    function enter() {
+        setState({ show: true })
+    }
+
+    createEffect(() => {
+        if (!state.parent) return
+
+        state.parent.addEventListener('mouseenter', enter)
+        state.parent.addEventListener('mouseleave', leave)
     })
 
     onCleanup(() => {
-        decoy.parentElement.removeEventListener('mouseenter', enter)
-        decoy.parentElement.removeEventListener('mouseleave', leave)
+        if (!state.parent) return
+
+        state.parent.removeEventListener('mouseenter', enter)
+        state.parent.removeEventListener('mouseleave', leave)
     })
 
     return (
-        <div class='cmp-tooltip-decoy' ref={decoy}>
-            <Show when={state.show}>
-                <Portal>
-                    <div
-                        class='cmp-tooltip'
-                        ref={tooltip}
-                        classList={{ bottom: state.pb }}
-                        style={{
-                            top: state.y + 'px',
-                            left: state.x + 'px',
-                            '--after-offset': state.p + 'px',
-                        }}
-                    >
-                        {P.children}
-                    </div>
-                </Portal>
-            </Show>
-        </div>
+        <Popup
+            show={state.show}
+            getProps={p => {
+                setState({ parent: p.parent })
+            }}
+        >
+            {P.children}
+        </Popup>
     )
 }
