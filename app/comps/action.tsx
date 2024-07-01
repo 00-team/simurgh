86d3fd879a9844165351df925fdaa76aa35c2a56
sadra <sadrataghavi1383@gmail.com>
2024-07-01 18:23:04 +0330

    feat(btns)

--- app/comps/action.tsx
+++ app/comps/action.tsx
@@ -19,7 +19,7 @@ type Props = (IconProps | TextProps) & {
 export const Action: Component<Props> = P => {
     return (
         <button
-            class='styled'
+            class='styled ttle_small'
             classList={{
                 icon: 'icon' in P,
                 active: P.active,
