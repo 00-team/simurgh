c463b076173baf26e8cf40f2d047d02830bb8faa
sadra <sadrataghavi1383@gmail.com>
2024-07-01 14:46:32 +0330

    feat(editor)

--- app/pages/editor/image.tsx
+++ app/pages/editor/image.tsx
@@ -50,46 +50,102 @@ export const EditorImageBlock: Component<Props> = P => {
 }
 
 const ImageUploader: Component<Props> = P => {
-    function upload_record() {
-        let el = document.createElement('input')
-        el.setAttribute('type', 'file')
-        el.setAttribute('accept', 'image/*')
-        el.onchange = () => {
-            if (!el.files || !el.files[0]) return
-            if (el.files[0].size > 209715200) {
-                addAlert({
-                    type: 'error',
-                    subject: 'حجم فایل',
-                    content:
-                        'حجم فایل مورد نظر بیشتر از حد مجاز است\nحداکثر حجم مجاز 200 مگابایت می باشد',
-                    timeout: 5,
-                })
-                return
-            }
-
-            let data = new FormData()
-            data.set('record', el.files[0], store.blog.title || store.blog.slug)
-
-            httpx({
-                url: `/api/projects/${store.blog.project}/records/`,
-                method: 'POST',
-                data,
-                onLoad(x) {
-                    if (x.status != 200) return
-                    setStore(
-                        produce(s => {
-                            let b = s.data[P.idx] as BlogImage
-                            b.record_salt = x.response.salt
-                            b.record_id = x.response.id
-                        })
-                    )
-                },
-            })
+    function upload_record(
+        e: Event & {
+            currentTarget: HTMLInputElement
+            target: HTMLInputElement
         }
-        el.click()
+    ) {
+        const el = e.target
+        if (!el.files || !el.files[0]) return
+        if (el.files[0].size > 209715200) {
+            addAlert({
+                type: 'error',
+                subject: 'حجم فایل',
+                content:
+                    'حجم فایل مورد نظر بیشتر از حد مجاز است\nحداکثر حجم مجاز 200 مگابایت می باشد',
+                timeout: 5,
+            })
+            return
+        }
+
+        let data = new FormData()
+        data.set('record', el.files[0], store.blog.title || store.blog.slug)
+
+        httpx({
+            url: `/api/projects/${store.blog.project}/records/`,
+            method: 'POST',
+            data,
+            onLoad(x) {
+                if (x.status != 200) return
+                setStore(
+                    produce(s => {
+                        let b = s.data[P.idx] as BlogImage
+                        b.record_salt = x.response.salt
+                        b.record_id = x.response.id
+                    })
+                )
+            },
+        })
     }
+
     return (
-        <div id='uploadArea' class='upload-area'>
+        <div
+            id='uploadArea'
+            class='upload-area'
+            ondragover={e => e.preventDefault()}
+            ondrop={e => {
+                console.log('slm')
+                e.preventDefault()
+                const files = e.dataTransfer.files
+
+                console.log(files)
+
+                Array.from(files).forEach(file => {
+                    if (file.type.match('image.*')) {
+                        const reader = new FileReader()
+                        reader.onload = () => {
+                            // Do something with the image data, e.g. display the image
+                            if (!files || !files[0]) return
+                            if (files[0].size > 209715200) {
+                                addAlert({
+                                    type: 'error',
+                                    subject: 'حجم فایل',
+                                    content:
+                                        'حجم فایل مورد نظر بیشتر از حد مجاز است\nحداکثر حجم مجاز 200 مگابایت می باشد',
+                                    timeout: 5,
+                                })
+                                return
+                            }
+
+                            let data = new FormData()
+                            data.set(
+                                'record',
+                                files[0],
+                                store.blog.title || store.blog.slug
+                            )
+
+                            httpx({
+                                url: `/api/projects/${store.blog.project}/records/`,
+                                method: 'POST',
+                                data,
+                                onLoad(x) {
+                                    if (x.status != 200) return
+                                    setStore(
+                                        produce(s => {
+                                            let b = s.data[P.idx] as BlogImage
+                                            b.record_salt = x.response.salt
+                                            b.record_id = x.response.id
+                                        })
+                                    )
+                                },
+                            })
+                        }
+                        reader.readAsDataURL(file)
+                    }
+                })
+            }}
+        >
             <div class='upload-area__header'>
                 <h1 class='upload-area__title title'>فایل خود را اپلود کنید</h1>
                 <p class='upload-area__paragraph description'>
@@ -97,10 +153,10 @@ const ImageUploader: Component<Props> = P => {
                 </p>
             </div>
 
-            <div
+            <label
                 id='dropZoon'
                 class='upload-area__drop-zoon drop-zoon'
-                onclick={upload_record}
+                for='img-input'
             >
                 <div class='drop-zoon__icon icon'>
                     <ImageIcon size={60} />
@@ -108,9 +164,6 @@ const ImageUploader: Component<Props> = P => {
                 <p class='drop-zoon__paragraph title_smaller'>
                     فایل خود را اینجا بندازید یا کلیک کنید.
                 </p>
-                <span id='loadingText' class='drop-zoon__loading-text'>
-                    صبر کنید...
-                </span>
                 <img
                     src=''
                     alt='Preview Image'
@@ -118,7 +171,13 @@ const ImageUploader: Component<Props> = P => {
                     class='drop-zoon__preview-image'
                     draggable='false'
                 />
-            </div>
+                <input
+                    type='file'
+                    accept='image/*'
+                    id='img-input'
+                    onchange={e => upload_record(e)}
+                />
+            </label>
         </div>
     )
 }
