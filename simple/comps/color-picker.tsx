965fa41c5d871cc5936be5813dd2e0f8f1f7b08b
007 <dr007cc@gmail.com>
2024-07-03 04:47:03 +0330

    fix(simple): color picker

--- simple/comps/color-picker.tsx
+++ simple/comps/color-picker.tsx
@@ -1,8 +1,15 @@
-import { Component, createEffect, on, onCleanup, onMount } from 'solid-js'
+import {
+    Component,
+    createEffect,
+    createMemo,
+    on,
+    onCleanup,
+    onMount,
+} from 'solid-js'
 import { Popup } from './popup'
 
 import './style/color-picker.scss'
-import { createStore, produce } from 'solid-js/store'
+import { createStore } from 'solid-js/store'
 
 type Props = {
     default?: string
@@ -14,35 +21,28 @@ export const ColorPicker: Component<Props> = P => {
         x: number
         y: number
         active: boolean
-        hsva: HSVA
-        hue: string
-        colorna: string
+        alpha: number
+        hsv: HSV
     }
     const [state, setState] = createStore<State>({
         x: 0,
         y: 0,
         active: false,
-        hsva: {
+        alpha: 0,
+        hsv: {
             h: 0,
             s: 100,
             v: 100,
-            a: 100,
-        },
-        get hue() {
-            return RGBAToStr(
-                HSVAtoRGBA({
-                    h: this.hsva.h,
-                    s: 100,
-                    v: 100,
-                    a: this.hsva.a,
-                })
-            )
-        },
-        get colorna() {
-            return RGBAToStr(HSVAtoRGBA({ ...this.hsva, a: 100 }))
         },
     })
     let ref: HTMLDivElement
+
+    const rgb = createMemo(() => HSVtoRGB(state.hsv))
+    const rgb_str = createMemo(() => RGBToStr(rgb()))
+    const hex = createMemo(() => RGBToHex(rgb(), state.alpha))
+    const gradient_bg = createMemo(() =>
+        RGBToStr(HSVtoRGB({ h: state.hsv.h, s: 100, v: 100 }), state.alpha)
+    )
 
     function inactive() {
         setState({ active: false })
@@ -66,7 +66,7 @@ export const ColorPicker: Component<Props> = P => {
             y = (e.clientY - top) * (100 / (bottom - top))
         }
 
-        setState(s => ({ x, y, hsva: { ...s.hsva, s: x, v: 100 - y } }))
+        setState(s => ({ x, y, hsv: { ...s.hsv, s: x, v: 100 - y } }))
     }
 
     createEffect(() => {
@@ -78,31 +78,28 @@ export const ColorPicker: Component<Props> = P => {
     })
 
     createEffect(() => {
-        // if (!P.default || P.default[0] != '#') return
-        // console.log(P.default)
-        // let [r, g, b, a] = P.default
-        //     .slice(1)
-        //     .match(/.{2}/g)
-        //     .map(h => parseInt(h, 16))
-        //
-        // a ||= 255
-        // a /= 2.55
-        //
-        // setState(
-        //     produce(s => {
-        //         s.hsva = RGBAtoHSVA({ r, g, b, a })
-        //         s.x = s.hsva.s
-        //         s.y = 100 - s.hsva.v
-        //     })
-        // )
+        if (!P.default) return
+
+        if (P.default.startsWith('rgb')) {
+            let [rgb, alpha] = StrToRGB(P.default)
+            if (RGBToHex(rgb, alpha) != hex()) {
+                let hsv = RGBtoHSV(rgb)
+                setState({ alpha, hsv, x: hsv.s, y: 100 - hsv.v })
+            }
+            return
+        }
+
+        if (P.default.startsWith('#') && P.default != hex()) {
+            let [rgb, alpha] = HexToRGB(P.default)
+            let hsv = RGBtoHSV(rgb)
+            setState({ alpha, hsv, x: hsv.s, y: 100 - hsv.v })
+        }
     })
 
     createEffect(
         on(
-            () => state.hsva,
-            () => {
-                P.onChange(RGBAToHex(HSVAtoRGBA(state.hsva)))
-            },
+            () => hex(),
+            () => P.onChange(hex()),
             { defer: true }
         )
     )
@@ -112,14 +109,14 @@ export const ColorPicker: Component<Props> = P => {
             <div
                 class='cmp-color-picker'
                 onClick={e => e.stopPropagation()}
-                style={{ '--colorna': state.colorna }}
+                style={{ '--rgb': rgb_str() }}
             >
                 <div
                     class='ccp-gradient'
                     ref={ref}
                     onClick={move}
                     onMouseDown={() => setState({ active: true })}
-                    style={{ '--hue': state.hue, '--a': state.hsva.a + '%' }}
+                    style={{ '--hue': gradient_bg(), '--a': state.alpha + '%' }}
                 >
                     <div
                         class='ccp-dot'
@@ -128,14 +125,15 @@ export const ColorPicker: Component<Props> = P => {
                 </div>
                 <Silinder
                     class='hue'
+                    default={state.hsv.h / 3.6}
                     onChange={h =>
-                        setState(s => ({ hsva: { ...s.hsva, h: h * 3.6 } }))
+                        setState(s => ({ hsv: { ...s.hsv, h: h * 3.6 } }))
                     }
                 />
                 <Silinder
                     class='alpha'
-                    default={state.hsva.a}
-                    onChange={a => setState(s => ({ hsva: { ...s.hsva, a } }))}
+                    default={state.alpha}
+                    onChange={alpha => setState({ alpha })}
                 />
             </div>
         </Popup>
@@ -199,17 +197,16 @@ const Silinder: Component<SilinderProps> = P => {
     )
 }
 
-type HSVA = {
+type HSV = {
     s: number
     v: number
     h: number
-    a: number
 }
-function HSVAtoRGBA(hsva: HSVA): RGBA {
-    const saturation = hsva.s / 100
-    const value = hsva.v / 100
+function HSVtoRGB({ h, s, v }: HSV): RGB {
+    const saturation = s / 100
+    const value = v / 100
     let chroma = saturation * value
-    let hueBy60 = hsva.h / 60
+    let hueBy60 = h / 60
     let x = chroma * (1 - Math.abs((hueBy60 % 2) - 1))
     let m = value - chroma
 
@@ -225,20 +222,18 @@ function HSVAtoRGBA(hsva: HSVA): RGBA {
         r: Math.round(red * 255),
         g: Math.round(green * 255),
         b: Math.round(blue * 255),
-        a: hsva.a,
     }
 }
 
-type RGBA = {
+type RGB = {
     r: number
     g: number
     b: number
-    a: number
 }
-function RGBAtoHSVA(rgba: RGBA): HSVA {
-    const red = rgba.r / 255
-    const green = rgba.g / 255
-    const blue = rgba.b / 255
+function RGBtoHSV({ r, g, b }: RGB): HSV {
+    const red = r / 255
+    const green = g / 255
+    const blue = b / 255
     const xmax = Math.max(red, green, blue)
     const xmin = Math.min(red, green, blue)
     const chroma = xmax - xmin
@@ -267,24 +262,71 @@ function RGBAtoHSVA(rgba: RGBA): HSVA {
         h: hue < 0 ? hue + 360 : hue,
         s: Math.round(saturation * 100),
         v: Math.round(value * 100),
-        a: rgba.a,
     }
 }
 
-function RGBAToHex(rgba: RGBA): string {
-    let R = rgba.r.toString(16).padStart(2, '0')
-    let G = rgba.g.toString(16).padStart(2, '0')
-    let B = rgba.b.toString(16).padStart(2, '0')
-    let A = ''
+function RGBToHex({ r, g, b }: RGB, alpha?: number): string {
+    let V =
+        '#' +
+        r.toString(16).padStart(2, '0') +
+        g.toString(16).padStart(2, '0') +
+        b.toString(16).padStart(2, '0')
 
-    if (rgba.a < 100) {
-        const alpha = (rgba.a * 2.55) | 0
-        A = alpha.toString(16).padStart(2, '0')
+    if (alpha != undefined && alpha < 100) {
+        V += ((alpha * 2.55) | 0).toString(16).padStart(2, '0')
     }
 
-    return '#' + R + G + B + A
+    return V
 }
 
-function RGBAToStr(rgba: RGBA): string {
-    return `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a}%)`
+function HexToRGB(hex: string): [RGB, number] {
+    let rgb = { r: 0, g: 0, b: 0 }
+    let alpha = 100
+
+    if (hex.length < 3 || hex.length > 9) return [rgb, alpha]
+    if (hex.startsWith('#')) hex = hex.slice(1)
+    if (hex.length == 5 || hex.length == 7) return [rgb, alpha]
+
+    if (hex.length < 6) {
+        let [r, g, b, a] = hex.match(/.{1}/g).map(c => parseInt(c + c, 16))
+        rgb = { r, g, b }
+        alpha = a || 255
+    } else {
+        let [r, g, b, a] = hex.match(/.{2}/g).map(c => parseInt(c, 16))
+        rgb = { r, g, b }
+        alpha = a || 255
+    }
+    return [rgb, alpha / 255]
+}
+
+function RGBToStr({ r, g, b }: RGB, alpha?: number): string {
+    if (alpha != undefined && alpha < 100)
+        return `rgba(${r}, ${g}, ${b}, ${alpha}%)`
+    else return `rgb(${r}, ${g}, ${b})`
+}
+
+function StrToRGB(str: string): [RGB, number] {
+    str = str.trim()
+    let rgb = { r: 0, g: 0, b: 0 }
+    let alpha = 100
+
+    if (!str.startsWith('rgb')) return [rgb, alpha]
+
+    let [kind, r, g, b, a] = str
+        .slice(3)
+        .replace(/\(|\)|,/g, '|')
+        .split('|')
+    rgb.r = parseInt(r) || 0
+    rgb.g = parseInt(g) || 0
+    rgb.b = parseInt(b) || 0
+
+    if (kind.startsWith('a') && a) {
+        if (a.includes('%')) {
+            alpha = parseFloat(a.replace('%', '')) || 100
+        } else {
+            alpha = (parseFloat(a) || 1) * 100
+        }
+    }
+
+    return [rgb, alpha]
 }
