2e56dde08fbdbacf5f96285e89cb9c558e8c6eb4
sadra <sadrataghavi1383@gmail.com>
2024-07-06 18:13:09 +0330

    feat(popup): types

--- app/comps/popup.tsx
+++ app/comps/popup.tsx
@@ -2,6 +2,18 @@ import { Component } from 'solid-js'
 
 import './style/popup.scss'
 
+type popupTypes = 'success' | 'info' | 'error'
+
+interface PopupProps {
+    type: popupTypes
+    content: string
+    title: string
+}
+
 export const Popup: Component = () => {
-    return <div class='popup'></div>
+    return (
+        <div class='popup'>
+            <div class='popup-wrapper'></div>
+        </div>
+    )
 }
