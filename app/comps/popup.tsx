a7b37698af590e18cadfddba7872af4547e2a38f
sadra <sadrataghavi1383@gmail.com>
2024-07-07 14:08:20 +0330

    feat(popup):

--- app/comps/popup.tsx
+++ app/comps/popup.tsx
@@ -1,19 +1,35 @@
 import { Component } from 'solid-js'
+import { popup } from 'store/popup'
 
 import './style/popup.scss'
 
-type popupTypes = 'success' | 'info' | 'error'
-
-interface PopupProps {
-    type: popupTypes
-    content: string
-    title: string
-}
-
-export const Popup: Component = () => {
+export const Popup: Component = P => {
     return (
-        <div class='popup'>
-            <div class='popup-wrapper'></div>
+        <div class='popup' classList={{ show: popup.show }}>
+            <form
+                onsubmit={e => {
+                    e.preventDefault()
+                    popup.onSubmit()
+                }}
+                class='popup-wrapper'
+            >
+                {popup.Icon && <popup.Icon />}
+
+                <h3 class='title'>{popup.title}</h3>
+                <p class='title_smaller'>{popup.content}</p>
+
+                <div class='ctas'>
+                    <button class='cta submit title_small'>تایید</button>
+                    <button
+                        class='cta reject title_small'
+                        onclick={() => {
+                            popup.onReject()
+                        }}
+                    >
+                        لغو
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
