d7a345dfd27b03a1e7d09a0931325e81087d49b6
sadra <sadrataghavi1383@gmail.com>
2024-07-10 13:09:13 +0330

    feat(categories)

--- app/pages/categories.tsx
+++ app/pages/categories.tsx
@@ -1,10 +1,43 @@
-import { useNavigate, useParams } from '@solidjs/router'
+import { useNavigate, useParams, useSearchParams } from '@solidjs/router'
 import { ArrowLeftIcon } from 'icons'
+import { BlogCategory } from 'models'
+import { httpx } from 'shared'
+import { createEffect } from 'solid-js'
+import { createStore } from 'solid-js/store'
 import './style/categories.scss'
 
 export default () => {
     const { pid } = useParams()
     const nav = useNavigate()
+    const [params, setParams] = useSearchParams()
+
+    type State = {
+        categories: BlogCategory[]
+        page: number
+    }
+
+    const [state, setState] = createStore<State>({
+        categories: [],
+        page: parseInt(params.page) || 0,
+    })
+
+    createEffect(() => {
+        category_list(state.page)
+        setParams({ page: state.page })
+    })
+
+    function category_list(page: number) {
+        httpx({
+            url: `/api/projects/${pid}/blog-categories/`,
+            method: 'GET',
+            params: { page },
+            onLoad(x) {
+                if (x.status != 200) return
+
+                setState({ categories: x.response, page })
+            },
+        })
+    }
 
     return (
         <section class='categories'>
@@ -31,47 +64,28 @@ export default () => {
                             <th class='count'>تعداد استفاده</th>
                         </tr>
                     </thead>
-                    <tbody class='title_smaller'>
-                        <tr>
-                            <td>1</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                            <td>d</td>
-                        </tr>
+                    <tbody
+                        class='title_smaller'
+                        classList={{ empty: state.categories.length <= 0 }}
+                    >
+                        {state.categories.length >= 1 ? (
+                            <>
+                                {state.categories.map(C => {
+                                    return (
+                                        <tr>
+                                            <td>{C.id}</td>
+                                            <td>{C.slug}</td>
+                                            <td>{C.label}</td>
+                                            <td>{C.detail}</td>
+                                            <td>{C.project}</td>
+                                            <td>{C.count}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </>
+                        ) : (
+                            <tr class='empty'></tr>
+                        )}
                     </tbody>
                 </table>
             </div>
