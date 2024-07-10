cf0e0e63dadb998e8669d807e758c86313a30ca8
sadra <sadrataghavi1383@gmail.com>
2024-07-10 12:57:16 +0330

    feat(categories)

--- app/pages/categories.tsx
+++ app/pages/categories.tsx
@@ -11,12 +11,69 @@ export default () => {
             <div class='actions'>
                 <div class='ctas'>
                     <button
-                        class='  go-back icon'
+                        class='go-back icon'
                         onClick={() => nav('/projects/' + pid)}
                     >
                         <ArrowLeftIcon size={25} />
                     </button>
                 </div>
+            </div>
+            <div class='categories-wrapper'>
+                <h3 class='section_title'>دسته بندی مقاله</h3>
+                <table>
+                    <thead class='title_smaller'>
+                        <tr>
+                            <th class='id'>شماره</th>
+                            <th class='slug'>نشانه</th>
+                            <th class='label'>نام</th>
+                            <th class='detail'>توضیح </th>
+                            <th class='project'>پروژه</th>
+                            <th class='count'>تعداد استفاده</th>
+                        </tr>
+                    </thead>
+                    <tbody class='title_smaller'>
+                        <tr>
+                            <td>1</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                            <td>d</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     )
