afed4b9b9866a80ae0d2f8003a8e2684b943c856
sadra <sadrataghavi1383@gmail.com>
2024-07-11 17:48:45 +0330

    feat(audio)

--- app/pages/editor/video.tsx
+++ app/pages/editor/video.tsx
@@ -102,46 +102,65 @@ const VideoUploader: Component<Props> = P => {
                 const files = e.dataTransfer.files
 
                 Array.from(files).forEach(file => {
-                    // if (file.type.match('image.*')) {
-                    //     const reader = new FileReader()
-                    //     reader.onload = () => {
-                    //         // Do something with the image data, e.g. display the image
-                    //         if (!files || !files[0]) return
-                    //         if (files[0].size > 209715200) {
-                    //             addAlert({
-                    //                 type: 'error',
-                    //                 subject: 'حجم فایل',
-                    //                 content:
-                    //                     'حجم فایل مورد نظر بیشتر از حد مجاز است\nحداکثر حجم مجاز 200 مگابایت می باشد',
-                    //                 timeout: 5,
-                    //             })
-                    //             return
-                    //         }
-                    //         let data = new FormData()
-                    //         data.set(
-                    //             'record',
-                    //             files[0],
-                    //             store.blog.title || store.blog.slug
-                    //         )
-                    //         httpx({
-                    //             url: `/api/projects/${store.blog.project}/records/`,
-                    //             method: 'POST',
-                    //             data,
-                    //             onLoad(x) {
-                    //                 if (x.status != 200) return
-                    //                 let r: RecordModel = x.response
-                    //                 setStore(
-                    //                     produce(s => {
-                    //                         let b = s.data[P.idx] as BlogImage
-                    //                         b.record_id = r.id
-                    //                         b.url = `/simurgh-record/r-${r.id}-${r.salt}`
-                    //                     })
-                    //                 )
-                    //             },
-                    //         })
-                    //     }
-                    //     reader.readAsDataURL(file)
-                    // }
+                    if (!file.type.startsWith('video/'))
+                        return addAlert({
+                            type: 'error',
+                            subject: 'نوع فایل',
+                            content: 'نوع فایل به صورت فیلم باشد!',
+                            timeout: 5,
+                        })
+
+                    const reader = new FileReader()
+
+                    reader.onload = () => {
+                        // Do something with the image data, e.g. display the image
+                        if (!files || !files[0]) return
+                        if (files[0].size > 209715200) {
+                            addAlert({
+                                type: 'error',
+                                subject: 'حجم فایل',
+                                content:
+                                    'حجم فایل مورد نظر بیشتر از حد مجاز است\nحداکثر حجم مجاز 200 مگابایت می باشد',
+                                timeout: 5,
+                            })
+                            return
+                        }
+
+                        let usage: RecordUsages = {
+                            kind: 'blog',
+                            id: store.blog.id,
+                        }
+                        let data = new FormData()
+                        data.set(
+                            'record',
+                            file,
+                            store.blog.title || store.blog.slug
+                        )
+                        data.set(
+                            'usage',
+                            new Blob([JSON.stringify(usage)], {
+                                type: 'application/json',
+                            })
+                        )
+
+                        httpx({
+                            url: `/api/projects/${store.blog.project}/records/`,
+                            method: 'POST',
+                            data,
+                            onLoad(x) {
+                                if (x.status != 200) return
+                                let r: RecordModel = x.response
+                                setStore(
+                                    produce(s => {
+                                        let b = s.data[P.idx] as BlogVideo
+                                        b.record_id = r.id
+                                        b.url = `/simurgh-record/r-${r.id}-${r.salt}`
+                                    })
+                                )
+                            },
+                        })
+                    }
+                    reader.readAsDataURL(file)
                 })
             }}
         >
