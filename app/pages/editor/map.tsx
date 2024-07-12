b91c5e03452f78c3ede7ad8bef83be20e7f3613d
sadra <sadrataghavi1383@gmail.com>
2024-07-12 16:09:57 +0330

    feat(editor)

--- app/pages/editor/map.tsx
+++ app/pages/editor/map.tsx
@@ -1,11 +1,13 @@
-import { BlogImage, BlogMap } from 'models'
-import { Component, onCleanup, onMount } from 'solid-js'
+import { BlogMap } from 'models'
+import { Component, createEffect, onCleanup, onMount, Show } from 'solid-js'
 
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-import './style/map.scss'
+import { SaveIcon } from 'icons'
 import { createStore } from 'solid-js/store'
+import './style/map.scss'
+import { setPopup } from 'store/popup'
 
 type Props = {
     idx: number
@@ -21,8 +23,47 @@ L.Icon.Default.mergeOptions({
 })
 
 export const EditorMapBlock: Component<Props> = P => {
+    return (
+        <div class='block-map'>
+            <Show
+                when={P.block.latitude !== 0 && P.block.longitude !== 0}
+                fallback={<MapNewMarker {...P} />}
+            >
+                <MapWIthMarker {...P} />
+            </Show>
+        </div>
+    )
+}
+
+const MapWIthMarker: Component<Props> = P => {
     let map
-    let currentMarker
+
+    onMount(() => {
+        map = L.map('map').setView([P.block.latitude, P.block.longitude], 13)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map)
+
+        L.marker([P.block.latitude, P.block.longitude]).addTo(map)
+
+        onCleanup(() => {
+            map.remove()
+        })
+    })
+    return (
+        <div
+            id='map'
+            style={{ height: '500px', width: 'clamp(0px,100%,1500px)' }}
+        ></div>
+    )
+}
+
+const MapNewMarker: Component<Props> = () => {
+    const [marker, setMarker] = createStore({ lat: '', lng: '' })
+    let map
+    let oldMarker
 
     onMount(() => {
         map = L.map('map').setView([35.6997005458638, 51.337867620465936], 13)
@@ -33,10 +74,17 @@ export const EditorMapBlock: Component<Props> = P => {
         }).addTo(map)
 
         map.on('click', function (e) {
-            if (currentMarker) {
-                map.removeLayer(currentMarker)
+            if (oldMarker) {
+                map.removeLayer(oldMarker)
             }
-            currentMarker = L.marker(e.latlng).addTo(map)
+            setMarker(e.latlng)
+        })
+
+        createEffect(() => {
+            if (oldMarker) {
+                map.removeLayer(oldMarker)
+            }
+            oldMarker = L.marker(marker).addTo(map)
         })
 
         onCleanup(() => {
@@ -44,13 +92,29 @@ export const EditorMapBlock: Component<Props> = P => {
         })
     })
     return (
-        <div class='block-map'>
+        <>
             <h3 class='title'>نقطه روی نقشه رو انتخاب کنید</h3>
             <p class='title_smaller'>با کلیک کردن میتونید نقطه رو عوض کنید.</p>
+            <button
+                class='cta save title_smaller'
+                classList={{ disable: marker.lat === '' && marker.lng === '' }}
+                onclick={() => {
+                    setPopup({
+                        show: true,
+                        content: 'از ذخیره نقطه مطمعنید؟',
+                        Icon: SaveIcon,
+                        title: 'ذخیره نقشه',
+                        type: 'info',
+                    })
+                }}
+            >
+                <SaveIcon />
+                ذخیر نقطه
+            </button>
             <div
                 id='map'
                 style={{ height: '500px', width: 'clamp(0px,100%,1500px)' }}
             ></div>
-        </div>
+        </>
     )
 }
