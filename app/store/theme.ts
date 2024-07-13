eb31b8c078a54065abd8bf690a8af06aca932aad
007 <dr007cc@gmail.com>
2024-07-13 13:59:31 +0330

    fix(app): update theme switch

--- app/store/theme.ts
+++ app/store/theme.ts
@@ -1,3 +1,26 @@
-import { createSignal } from 'solid-js'
+import { createEffect, createRoot, createSignal } from 'solid-js'
 
-export const [theme, setTheme] = createSignal<'light' | 'dark'>('dark')
+const ALLOWD_THEMES = ['light', 'dark'] as const
+type Theme = (typeof ALLOWD_THEMES)[number]
+
+function default_theme(): Theme {
+    let theme = localStorage.getItem('default_theme') as Theme
+    if (ALLOWD_THEMES.includes(theme)) {
+        return theme
+    }
+
+    if (matchMedia && matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark'
+    } else {
+        return 'light'
+    }
+}
+
+export const [theme, setTheme] = createSignal<Theme>(default_theme())
+
+createRoot(() => {
+    createEffect(() => {
+        localStorage.setItem('default_theme', theme())
+        document.documentElement.setAttribute('data-theme', theme())
+    })
+})
