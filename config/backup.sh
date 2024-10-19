bc6c8d7b33df5164d39d8b447b5b030909f41550
007 <dr007cc@gmail.com>
2024-10-19 09:20:17 +0330

    fix: update backup.sh

--- config/backup.sh
+++ config/backup.sh
@@ -2,10 +2,11 @@
 
 BASE_URL="https://api.telegram.org/bot$TELOXIDE_TOKEN"
 BASE_NAME=simurgh-db-$(date +%s).tgz.
-HASH_FILENAME=".ignore.last_hash"
+DB_CHANGED=".ignore.db-changed"
+RECORD_CHANGED=".ignore.record-changed"
+RECORD_NAME=simurgh-record-$(date +%s).tgz.
 CHAT_INFO="chat_id=$TELOXIDE_GROUP_ID&message_thread_id=2"
 
-mkdir .dbd -p
 
 function send_file {
     url="$BASE_URL/sendDocument?$CHAT_INFO"
@@ -17,23 +18,54 @@ function send_msg {
     curl -s -X GET $url --data-urlencode "text=$1" -o /dev/null
 }
 
-if [ -f $HASH_FILENAME ]; then
-    if [[ $(cat $HASH_FILENAME) == $(sha256sum main.db) ]]; then
-        echo "database has not changed 🪐 $(date +'%F %T')"
-        exit
+function backup_db {
+    if [ -f $DB_CHANGED ]; then
+        if [[ $(cat $DB_CHANGED) == $(sha256sum main.db) ]]; then
+            echo "database has not changed 🪐 $(date +'%F %T')"
+            return;
+        fi
     fi
-fi
 
-sha256sum main.db > $HASH_FILENAME
+    mkdir .dbd -p
+    sha256sum main.db > $DB_CHANGED
+    send_msg "starting the db backup $(date +'%F %T')"
+    tar czvf - main.db | split -d -b 40MB - .dbd/$BASE_NAME
+    cd .dbd
+    for f in $BASE_NAME*; do
+        send_file $f
+        sleep 1
+    done
+    send_msg "end of backup 🐧"
+    cd ..
+    rm -rf .dbd
+}
 
-send_msg "starting the backup $(date +'%F %T')"
-tar czvf - main.db record | split -d -b 40MB - .dbd/$BASE_NAME
-cd .dbd
-for f in $BASE_NAME*; do
-    send_file $f
-    sleep 1
-done
-send_msg "end of backup 🐧"
-cd ..
-rm -rf .dbd
+function record_change {
+    { ls record; du -sb record; } | sha256sum
+}
+
+function backup_record {
+    if [ -f $RECORD_CHANGED]; then
+        if [[ $(cat $RECORD_CHANGED) == $(record_change) ]]; then
+            echo "record has not changed 🪐 $(date +'%F %T')"
+            return;
+        fi
+    fi
+
+    mkdir .rbd -p
+    record_change > $RECORD_CHANGED
+    send_msg "starting the reocrd backup $(date +'%F %T') 📂"
+    tar czvf - record | split -d -b 40MB - .rbd/$RECORD_NAME
+    cd .rbd
+    for f in $RECORD_NAME*; do
+        send_file $f
+        sleep 1
+    done
+    send_msg "end of record backup 🐧"
+    cd ..
+    rm -rf .rbd
+}
+
+backup_db;
+backup_record;
 
