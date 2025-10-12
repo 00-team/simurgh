2b00d861aa1f43bc682188e7008c2ff3b3fdc871
007 <dr007cc@gmail.com>
2025-10-13 00:47:00 +0330

    fix: bk

--- config/backup.sh
+++ config/backup.sh
@@ -15,7 +15,7 @@ function send_file {
 `'"$h"'`'
     url="$BASE_URL/send-file/"
     curl -s -X POST $url -F "file=@$1" -F "text=$c" \
-        -F "channel=simurgh" -F "pass=$IRIS_PASS" -F "parse_mode=MarkdownV2" # -o /dev/null
+        -F "channel=simurgh" -F "pass=$IRIS_PASS" -F "parse_mode=MarkdownV2" -o /dev/null
     echo
 }
 
@@ -23,7 +23,7 @@ function send_msg {
     url="$BASE_URL/send-mp/"
     curl -s -X POST $url -F "channel=simurgh" \
         -F "pass=$IRIS_PASS" -F "text=$1" \
-        -F "parse_mode=MarkdownV2" # -o /dev/null
+        -F "parse_mode=MarkdownV2" -o /dev/null
 }
 
 
@@ -34,7 +34,7 @@ function backup_data {
     tar czvf $BASE_NAME main.db record
     send_msg 'spliting and sending the data `'"$(du -sh $BASE_NAME)"'`
 `'"$(date +'%F %T')"'`'
-    split -d -b 45M $BASE_NAME "$OUT_DIR/$BASE_NAME."
+    split -d -b 40M $BASE_NAME "$OUT_DIR/$BASE_NAME."
 
     # tar czvf - data/ | split -d -b 45MB - $OUT_DIR/$BASE_NAME
     cd $OUT_DIR
