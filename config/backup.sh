c53c3f32a6c72c4c92584059d46f21d33fbe857c
007 <dr007cc@gmail.com>
2025-10-12 23:08:56 +0330

    fix: backup

--- config/backup.sh
+++ config/backup.sh
@@ -1,71 +1,51 @@
 #!/bin/bash
 
-BASE_URL="https://api.telegram.org/bot$TELOXIDE_TOKEN"
-BASE_NAME=simurgh-db-$(date +%s).tgz.
-DB_CHANGED=".ignore.db-changed"
-RECORD_CHANGED=".ignore.record-changed"
-RECORD_NAME=simurgh-record-$(date +%s).tgz.
-CHAT_INFO="chat_id=$TELOXIDE_GROUP_ID&message_thread_id=2"
+BASE_URL="https://iris.00-team.org/api/abzar"
+BASE_NAME=simurgh-data-$(date +%s).tgz
+OUT_DIR=".ignore.data"
 
 
 function send_file {
-    url="$BASE_URL/sendDocument?$CHAT_INFO"
-    curl -s -X POST $url -F "document=@$1" -F "caption=$1" -o /dev/null
+    echo "sending $(du -sb $1) to telegram"
+    s="$(du -sb $1)"
+    h="$(sha256sum -b $1)"
+    h="${h% *}"
+    c='`'"$s"'`
+
+`'"$h"'`'
+    url="$BASE_URL/send-file/"
+    curl -s -X POST $url -F "file=@$1" -F "text=$c" \
+        -F "channel=simurgh" -F "pass=$IRIS_PASS" -F "parse_mode=MarkdownV2" -o /dev/null
+    echo
 }
 
 function send_msg {
-    url="$BASE_URL/sendMessage?$CHAT_INFO"
-    curl -s -X GET $url --data-urlencode "text=$1" -o /dev/null
+    url="$BASE_URL/send-mp/"
+    curl -s -X POST $url -F "channel=hamrah" \
+        -F "pass=$IRIS_PASS" -F "text=$1" \
+        -F "parse_mode=MarkdownV2" -o /dev/null
 }
 
-function backup_db {
-    if [ -f $DB_CHANGED ]; then
-        if [[ $(cat $DB_CHANGED) == $(sha256sum main.db) ]]; then
-            echo "database has not changed 🪐 $(date +'%F %T')"
-            return;
-        fi
-    fi
 
-    mkdir .dbd -p
-    sha256sum main.db > $DB_CHANGED
-    send_msg "starting the db backup $(date +'%F %T')"
-    tar czvf - main.db | split -d -b 40MB - .dbd/$BASE_NAME
-    cd .dbd
+function backup_data {
+    mkdir $OUT_DIR -p
+    send_msg 'starting the compression: `'"$(du -sh main.db record)"'`
+`'"$(date +'%F %T')"'`'
+    tar czvf $BASE_NAME main.db record
+    send_msg 'spliting and sending the data `'"$(du -sh $BASE_NAME)"'`
+`'"$(date +'%F %T')"'`'
+    split -d -b 49M $BASE_NAME "$OUT_DIR/$BASE_NAME."
+
+    # tar czvf - data/ | split -d -b 45MB - $OUT_DIR/$BASE_NAME
+    cd $OUT_DIR
     for f in $BASE_NAME*; do
         send_file $f
         sleep 1
     done
-    send_msg "end of backup 🐧"
+    send_msg "\=\=\= End of Data backup \=\=\="
     cd ..
-    rm -rf .dbd
+    rm -rf $OUT_DIR
+    rm -rf $BASE_NAME
 }
 
-function record_change {
-    { ls record; du -sb record; } | sha256sum
-}
-
-function backup_record {
-    if [ -f $RECORD_CHANGED ]; then
-        if [[ $(cat $RECORD_CHANGED) == $(record_change) ]]; then
-            echo "record has not changed 🪐 $(date +'%F %T')"
-            return;
-        fi
-    fi
-
-    mkdir .rbd -p
-    record_change > $RECORD_CHANGED
-    send_msg "starting the reocrd backup $(date +'%F %T') 📂"
-    tar czvf - record | split -d -b 40MB - .rbd/$RECORD_NAME
-    cd .rbd
-    for f in $RECORD_NAME*; do
-        send_file $f
-        sleep 1
-    done
-    send_msg "end of record backup 🐧"
-    cd ..
-    rm -rf .rbd
-}
-
-backup_db;
-backup_record;
-
+backup_data;
