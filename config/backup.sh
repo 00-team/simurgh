419d0dece288cefbebcbd3f4d71811a2b4dca717
007 <dr007cc@gmail.com>
2025-10-13 00:56:06 +0330

    fix: update

--- config/backup.sh
+++ config/backup.sh
@@ -31,11 +31,11 @@ function backup_data {
     mkdir $OUT_DIR -p
     send_msg 'starting the compression:
 `'"$(du -sh main.db record)"'`
-'"$(date +'%F %T')"
+`'"$(date +'%F %T')"'`'
     tar czvf $BASE_NAME main.db record
     send_msg 'spliting and sending the data
 `'"$(du -sh $BASE_NAME)"'`
-'"$(date +'%F %T')"
+`'"$(date +'%F %T')"'`'
     split -d -b 40M $BASE_NAME "$OUT_DIR/$BASE_NAME."
 
     # tar czvf - data/ | split -d -b 45MB - $OUT_DIR/$BASE_NAME
